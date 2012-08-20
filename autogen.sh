b1ad3e740f4da9440908899239c866abfd468ea8
Dana Burkart <discodancemonkey@gmail.com>
2012-08-19 17:44:35 -0700

    Markup is fun, but we don't need two readme files.
    This way github and autoconf get along.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Because we haven't written these files yet, and we'll get yelled at
-touch NEWS README AUTHORS ChangeLog
+touch NEWS AUTHORS ChangeLog
 
 # Create Makefile.in and friends
 autoreconf --force --install
